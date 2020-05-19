5328bf50244253f4a7be50c6991199d53228e73b
Gouri Chandrasekharan <gouri.chandrasekharan@incture.com>
2020-05-19 07:58:10 +0000

    Gouri/19May2020/1328/Launchpad & Home Page addition

--- webapp/controller/Launchpad.controller.js
+++ webapp/controller/Launchpad.controller.js
@@ -5,7 +5,15 @@ sap.ui.define([
 
 	return Controller.extend("com.sap.alj.sca.ALJ_SCA.controller.Launchpad", {
 		onInit: function () {
-
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		},
+		pressTile:function(evt){
+		//	var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		
+			if(evt.getSource().getId().includes("SCA_tile1")){
+				//navigate to home page
+					this.oRouter.navTo("Home");
+			}
 		}
 	});
 });
