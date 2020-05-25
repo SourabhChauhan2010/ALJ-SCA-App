6a189cc8b4b14a8a181839c4c92b19490f3ddf9a
Gouri Chandrasekharan <gouri.chandrasekharan@incture.com>
2020-05-25 05:45:03 +0000

    added book a service screen

--- webapp/controller/Launchpad.controller.js
+++ webapp/controller/Launchpad.controller.js
@@ -7,12 +7,14 @@ sap.ui.define([
 		onInit: function () {
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 		},
-		pressTile:function(evt){
-		//	var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-		
-			if(evt.getSource().getId().includes("SCA_tile1")){
+		pressTile: function (evt) {
+			//	var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+
+			if (evt.getSource().getId().includes("SCA_tile1")) {
 				//navigate to home page
-					this.oRouter.navTo("Home");
+				this.oRouter.navTo("Home");
+			} else {
+				this.oRouter.navTo("BookAService");
 			}
 		}
 	});
