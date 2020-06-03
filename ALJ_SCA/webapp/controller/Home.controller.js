7dfc9d36d94828217875a49b28202014549a5b07
Alagar93 <alagar.pandian@incture.com>
2020-06-03 05:28:33 +0000

    Home page navigations

--- ALJ_SCA/webapp/controller/Home.controller.js
+++ ALJ_SCA/webapp/controller/Home.controller.js
@@ -5,23 +5,29 @@ sap.ui.define([
 
 	return BaseController.extend("com.sap.alj.sca.ALJ_SCA.controller.Home", {
 		onInit: function () {
-				this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 		},
-		onAfterRendering:function(){
-		/*	this.byId("idVBServiceListItems").attachBrowserEvent("click", function(oEvent) {
-				this.NavtoServices();
-			});*/
+		onAfterRendering: function () {
+			/*	this.byId("idVBServiceListItems").attachBrowserEvent("click", function(oEvent) {
+					this.NavtoServices();
+				});*/
 		},
-		onBack:function(evt){
-				this.oRouter.navTo("Launchpad");
-			
+		onBack: function (evt) {
+			this.oRouter.navTo("Launchpad");
+
 		},
-		NavtoServices:function(evt){
-		var index=evt.getSource().getId().slice(42,43);
-		if(index==0){
-			this.oRouter.navTo("BookAService");	
+		NavtoServices: function (evt) {
+			var index = evt.getSource().getId().slice(41);
+			if (index === "0") {
+				this.oRouter.navTo("BookAService");
+			} else if (index === "1") {
+				this.oRouter.navTo("MyServices");
+			}
+		},
+
+		onBookService: function () {
+			this.oRouter.navTo("BookAService");
 		}
-		}
-	
+
 	});
 });
