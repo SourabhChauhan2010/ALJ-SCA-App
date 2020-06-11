ebe053e24b37cf1675b8606532e14ce48d0649fc
Alagar93 <Alagar.Pandian@incture.com>
2020-06-11 11:31:31 +0000

    product details without close button and home page web version added

---
+++ webapp/controller/ProductDetail.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+	"com/sap/alj/sca/ALJ_SCA/controller/BaseController"
+], function (BaseController) {
+	"use strict";
+
+	return BaseController.extend("com.sap.alj.sca.ALJ_SCA.controller.Profile", {
+
+		/**
+		 * Called when a controller is instantiated and its View controls (if available) are already created.
+		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+		 * @memberOf com.sap.alj.sca.ALJ_SCA.view.Profile
+		 */
+		onInit: function () {
+
+		}
+
+	});
+
+});
