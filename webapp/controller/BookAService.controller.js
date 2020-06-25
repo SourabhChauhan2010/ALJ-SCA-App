e7871f8ed2ada090d138c0ea01d6393cc9472920
Gouri Chandrasekharan <gouri.chandrasekharan@incture.com>
2020-06-25 07:56:36 +0000

    Book A Service Changes

--- webapp/controller/BookAService.controller.js
+++ webapp/controller/BookAService.controller.js
@@ -6,15 +6,43 @@ sap.ui.define([
 	return BaseController.extend("com.sap.alj.sca.ALJ_SCA.controller.BookAService", {
 		onInit: function () {
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+		},
+		onAfterRendering:function(){
+				this.servicelist=[];
+		},
+		onVehicleItemPress: function (oEvent) {
+			var oAppModel = this.getModel("oAppModel");
+			var aVehicles = oAppModel.getProperty("/vehicles");
+			var currObj = oEvent.getSource().getBindingContext("oAppModel").getObject();
+			for (var i = 0; i < aVehicles.length; i++) {
+				aVehicles[i].isSelected = false;
+			}
+			currObj.isSelected = true; //!isSelected
+			oAppModel.setProperty("/selectedVehicle", currObj); //isSelected ? false : currObj
+			oAppModel.refresh();
+			
+			//setting text in the receipt fragment
+			this.getView().getContent()[8].getItems()[0].getItems()[0].getItems()[1].setText(currObj.model + " " + currObj.year);
+			/*this.getView().byId("idBtnAddVeh").setVisible(false);
+			this.getView().byId("idPnHeaderVehInfo").setVisible(true);
+			this.getView().byId("idPnHeaderVehInfo").setText(currObj.model + " " + currObj.year);*/
+		},
+		onServiceItemPress:function(oEvent){
+			var currObj = oEvent.getSource().getBindingContext("oAppModel").getObject();
+			//setting text in the receipt fragment
+	
+			this.servicelist.push(currObj.serviceType);
+			this.getView().getContent()[8].getItems()[0].getItems()[1].getItems()[1].setText(this.servicelist);
+		
 			
 		},
-		onBeforeRendering:function(){
-			var oAppModelData = this.getModel("oAppModel").getData();	
+		onBeforeRendering: function () {
+			var oAppModelData = this.getModel("oAppModel").getData();
 		},
-		createAppointment:function(evt){
+		createAppointment: function (evt) {
 			//do mandatory checks
 			this.oRouter.navTo("ServiceStatus");
-			
+
 		}
 
 	});
