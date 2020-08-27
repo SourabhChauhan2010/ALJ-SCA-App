7c118b7f16a2b4b7405229ea44727b2349440a39
Pragnya Dash <pragnya.dash@incture.com>
2020-08-27 13:57:31 +0000

    Responsiveness of home page

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -6,7 +6,37 @@ sap.ui.define([
 	return BaseController.extend("com.sap.alj.sca.ALJ_SCA.controller.Home", {
 		onInit: function () {
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-
+            if (!sap.ui.Device.support.touch) {
+            this.getView().byId("idVboxHomeView").addStyleClass("sapUiMediumMarginBeginEnd");
+            this.getView().byId("idRecommedCusList").addStyleClass("aljReccListStyleDesktop");
+            this.getView().byId("idHomeWcomeTxt").addStyleClass("aljWelcomeTextCSSDesktop");
+            this.getView().byId("idMohMdTxt").addStyleClass("aljHomeNameTextCSSDesktop");
+            this.getView().byId("idLandCruiseTxt").addStyleClass("aljHomeCarModelTextCSSdesktop");
+            this.getView().byId("idKmTxt").addStyleClass("aljHomeTextCSSDesktop");
+            
+            this.getView().byId("idSwtchCar").addStyleClass("underLineClsDTop");
+            this.getView().byId("idBtnBookAServ").addStyleClass("aljServiceBtnStyleDTop");
+            this.getView().byId("idBtnMyServ").addStyleClass("aljServiceBtnStyleDTop");
+            this.getView().byId("idRecomdForTxt").addStyleClass("aljModelTextStyleDTop","sapUiTinyMarginTop");
+           
+            
+            }
+            else{
+            this.getView().byId("idVboxHomeView").removeStyleClass("sapUiMediumMarginBeginEnd");
+            this.getView().byId("idRecommedCusList").addStyleClass("aljReccListStyle");
+            this.getView().byId("idHomeWcomeTxt").addStyleClass("aljWelcomeTextCSS");
+            this.getView().byId("idMohMdTxt").addStyleClass("aljHomeNameTextCSS");
+            this.getView().byId("idLandCruiseTxt").addStyleClass("aljHomeCarModelTextCSS");
+            this.getView().byId("idKmTxt").addStyleClass("aljHomeTextCSS");
+            
+            this.getView().byId("idSwtchCar").addStyleClass("underLineCls");
+            this.getView().byId("idBtnBookAServ").addStyleClass("aljServiceBtnStyle");
+            this.getView().byId("idBtnMyServ").addStyleClass("aljServiceBtnStyleDTop");
+            this.getView().byId("idRecomdForTxt").addStyleClass("aljModelTextStyle","sapUiTinyMarginTop");
+            
+            
+            }
+            
 		},
 
 		onBack: function (evt) {
