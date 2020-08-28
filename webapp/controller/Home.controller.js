98ce41d78ba51f5fe99afc528bbdbb9090fb1ded
Pragnya Dash <pragnya.dash@incture.com>
2020-08-28 04:56:31 +0000

    added css classes in media queries

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -8,35 +8,10 @@ sap.ui.define([
 			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
             if (!sap.ui.Device.support.touch) {
             this.getView().byId("idVboxHomeView").addStyleClass("sapUiMediumMarginBeginEnd");
-            this.getView().byId("idRecommedCusList").addStyleClass("aljReccListStyleDesktop");
-            this.getView().byId("idHomeWcomeTxt").addStyleClass("aljWelcomeTextCSSDesktop");
-            this.getView().byId("idMohMdTxt").addStyleClass("aljHomeNameTextCSSDesktop");
-            this.getView().byId("idLandCruiseTxt").addStyleClass("aljHomeCarModelTextCSSdesktop");
-            this.getView().byId("idKmTxt").addStyleClass("aljHomeTextCSSDesktop");
-            
-            this.getView().byId("idSwtchCar").addStyleClass("underLineClsDTop");
-            this.getView().byId("idBtnBookAServ").addStyleClass("aljServiceBtnStyleDTop");
-            this.getView().byId("idBtnMyServ").addStyleClass("aljServiceBtnStyleDTop");
-            this.getView().byId("idRecomdForTxt").addStyleClass("aljModelTextStyleDTop","sapUiTinyMarginTop");
-           
-            
             }
             else{
             this.getView().byId("idVboxHomeView").removeStyleClass("sapUiMediumMarginBeginEnd");
-            this.getView().byId("idRecommedCusList").addStyleClass("aljReccListStyle");
-            this.getView().byId("idHomeWcomeTxt").addStyleClass("aljWelcomeTextCSS");
-            this.getView().byId("idMohMdTxt").addStyleClass("aljHomeNameTextCSS");
-            this.getView().byId("idLandCruiseTxt").addStyleClass("aljHomeCarModelTextCSS");
-            this.getView().byId("idKmTxt").addStyleClass("aljHomeTextCSS");
-            
-            this.getView().byId("idSwtchCar").addStyleClass("underLineCls");
-            this.getView().byId("idBtnBookAServ").addStyleClass("aljServiceBtnStyle");
-            this.getView().byId("idBtnMyServ").addStyleClass("aljServiceBtnStyleDTop");
-            this.getView().byId("idRecomdForTxt").addStyleClass("aljModelTextStyle","sapUiTinyMarginTop");
-            
-            
             }
-            
 		},
 
 		onBack: function (evt) {
